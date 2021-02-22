f908bd18d060dea9366d8b24697918d3b76d8af4
Elena Erofeeva <eerofeeva8@gmail.com>
2021-02-21 21:10:35 -0600

    WIP: working on importing data

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -1,1 +1,37 @@
 // @TODO: YOUR CODE HERE!
+
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+  var xAxis = "age";
+  var yAxis = "smokes";
+
+  //import data
+  d3.csv("data/data.csv").then(function(csvData) {
+      var smokes = csvData.smokes;
+      var age = csvData.age;
+      var stateAbbr = csvData.abbr;
+
+      
+  });
